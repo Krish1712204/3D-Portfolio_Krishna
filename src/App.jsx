0fe9889155971eb16139ae465573669899f1ff0e
Krishna Ramdas Shelar <krishnashelar2223_co@sanjivanicoe.org.in>
2026-05-26 18:24:45 +0530

    remove: remove Tech (3D balls) section from portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Education, Certification } from "./components";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Works, StarsCanvas, Education, Certification } from "./components";
 
 const App = () => {
   return (
@@ -12,7 +12,6 @@ const App = () => {
         </div>
         <About />
         <Experience />
-        <Tech />
         <Works />
         <Education />
         <Certification />
